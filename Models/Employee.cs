28a19af7f50cce9f8a6255586a5e585449d3cb33
Rawan Karroum <rawan.r.karroum@gmail.com>
2024-09-23 13:00:16 -0400

    inital commit with basic files

---
+++ Models/Employee.cs
@@ -0,0 +1,8 @@
+public class Employee
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Position { get; set; }
+    public decimal Salary { get; set; }
+}
